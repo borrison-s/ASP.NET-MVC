82c24d5dc205ad345a31a541d3a92a238047517f
Boris Stojkoski <boris.stojkoski@gmail.com>
2023-06-19 20:14:01 +0200

    Class02Homework added

---
+++ Class02Homework/SEDC.PizzaApp/SEDC.PizzaApp/Controllers/OrderController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Models;
+
+namespace SEDC.PizzaApp.Controllers
+{
+    public class OrderController : Controller
+    {
+        [Route("ListOrders")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult OrderDetails(int? id)
+        {
+            if (id == null)
+            {
+                return new EmptyResult();
+            }
+
+            Order order = StaticDb.Orders.FirstOrDefault(o => o.Id == id);
+            if (order == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(order);
+        }
+
+        public IActionResult JsonData()
+        {
+            Order order = new Order
+            {
+                Id = 3,
+                Price = 700
+            };
+
+            return new JsonResult(order);
+        }
+
+        public IActionResult RedirectToHomeIndex()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
---
+++ Class02Homework/SEDC.PizzaApp/SEDC.PizzaApp/Models/Order.cs
@@ -0,0 +1,8 @@
+﻿namespace SEDC.PizzaApp.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int Price { get; set; }
+    }
+}
